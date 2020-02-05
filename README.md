b4fe75907d7876971bbe97a831f7c4cdb340ca09
Francis Wright <59773998+fjwright@users.noreply.github.com>
2020-02-05 19:13:17 +0000

    Initial push to GitHub
    This is the first prototype that does anything useful!

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Run REDUCE
- A Java GUI to run any CLI version of the REDUCE CAS
+
+## A Java GUI to run any CLI version of the REDUCE CAS
+
+### Copyright &copy; Dr Francis J. Wright, February 2020
+
+This is the first prototype that does anything useful and my first
+ever check-in to GitHub! This GitHub repository is currently
+private. I will make it public when I have developed it a bit further.
+
+This version uses the Java Swing library, but at some future date I
+will probably switch to using JavaFX. Swing has the advantage that it
+is still a standard component of the latest Java SE SDK. I am using
+Java 13.
+
+This version only runs on Microsoft Windows, but I plan to add support
+for Linux. (Whilst Java is portable, filesystem structures and
+installation conventions are not!)
+
+This version only runs PSL REDUCE, but I plan to add support for the
+CLI version of CSL REDUCE and probably CL REDUCE.
---
+++ fjwright/runreduce/RunREDUCE.java
@@ -0,0 +1,201 @@
+/*
+ * Prototype Java GUI to run REDUCE using Swing.
+ * Based on https://docs.oracle.com/javase/tutorial/uiswing/index.html.
+ * This file is ../fjwright/runreduce/RunREDUCE.java
+ * Compile and run it from the PARENT directory of fjwright:
+ * javac fjwright/runreduce/RunREDUCE.java
+ * java fjwright.runreduce.RunREDUCE
+ * (The above works in a Windows cmd shell.)
+ */ 
+
+package fjwright.runreduce;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.text.*;
+import javax.swing.border.*;
+import java.io.*;
+
+/* RunREDUCE.java requires no other files. */
+
+public class RunREDUCE extends JPanel implements ActionListener {
+    public JTextArea outputTextArea, inputTextArea;
+    private final static String newline = "\n";
+    public static PrintWriter reduceInputPrintWriter;
+
+    public RunREDUCE() {
+        super(new BorderLayout()); // JPanel defaults to FlowLayout!
+
+        // Create the non-editable vertically-scrollable output text area:
+        outputTextArea = new JTextArea();
+        outputTextArea.setEditable(false);
+        JScrollPane outputScrollPane = new JScrollPane(outputTextArea);
+        outputScrollPane.setPreferredSize(new Dimension(640, 480));
+        outputScrollPane.setMinimumSize(new Dimension(120, 120));
+
+        // Create the editable vertically-scrollable input text area:
+        inputTextArea = new JTextArea();
+        JScrollPane inputScrollPane = new JScrollPane(inputTextArea);
+        inputScrollPane.setPreferredSize(new Dimension(640, 120));
+        inputScrollPane.setMinimumSize(new Dimension(120, 30));
+
+        // Create titled borders for the scrollpanes:
+        Border border = BorderFactory.createEtchedBorder(EtchedBorder.LOWERED);
+        TitledBorder titledBorder = BorderFactory.createTitledBorder(border, "Input/Output Display");
+        titledBorder.setTitlePosition(TitledBorder.ABOVE_TOP);
+        outputScrollPane.setBorder(titledBorder);
+        titledBorder = BorderFactory.createTitledBorder(border, "Input editor");
+        titledBorder.setTitlePosition(TitledBorder.ABOVE_TOP);
+        inputScrollPane.setBorder(titledBorder);
+
+        // Create a split pane to contain the output and input scrollpanes:
+        JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT,
+                                              outputScrollPane, inputScrollPane);
+        splitPane.setOneTouchExpandable(true);
+        splitPane.setResizeWeight(0.8);
+        add(splitPane, BorderLayout.CENTER);
+
+        // Create a button to input the text entered above:
+        JButton inputButton = new JButton("Input");
+        add(inputButton, BorderLayout.SOUTH);
+        inputButton.setActionCommand("input");
+        inputButton.addActionListener(this);
+        inputButton.setToolTipText("Send the input above to REDUCE. It is terminated with a newline if necessary.");
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if ("input".equals(e.getActionCommand())) {
+            // System.out.println("Input button clicked!");
+            String text = inputTextArea.getText();
+            if (!text.endsWith(newline)) text += newline;
+            inputTextArea.setText(null);
+            outputTextArea.append(text);
+            // Make sure the new text is visible, even if there was a
+            // selection in the text area:
+            outputTextArea.setCaretPosition(outputTextArea.getDocument().getLength());
+
+            // Send the input to the REDUCE input pipe:
+            reduceInputPrintWriter.print(text);
+            reduceInputPrintWriter.flush(); // Necessary?
+        }
+    }
+    
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
+     */
+    private static void createAndShowGUI() {
+        // Create and set up the window:
+        JFrame frame = new JFrame("RunREDUCE");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Add content to the window:
+        RunREDUCE runREDUCE = new RunREDUCE();
+        frame.add(runREDUCE);
+
+        // Create a menu bar:
+        JMenuBar menuBar = new JMenuBar();
+        frame.setJMenuBar(menuBar);
+        // menuBar.setOpaque(true);
+
+        // Create a Help menu:
+        JMenu menu = new JMenu("Help");
+        menuBar.add(menu);
+
+        // Create an About menuItem that pops up a dialogue:
+        JMenuItem menuItem = new JMenuItem("About");
+        menuItem.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    JOptionPane.showMessageDialog
+                        (frame,
+                         // "Prototype version 0.1\nFrancis Wright, February 2020",
+                         new String [] {"Run CLI REDUCE in a Java Swing GUI.",
+                                        "Prototype version 0.1",
+                                        "Francis Wright, February 2020"},
+                         "About RunREDUCE",
+                         JOptionPane.PLAIN_MESSAGE);
+                }
+            });
+        menu.add(menuItem);
+
+        // Display the window:
+        frame.pack();
+        frame.setVisible(true);
+
+        // Run REDUCE.  A direct call hangs the GUI!
+        SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                    reduce(runREDUCE);
+                }
+            });
+    }
+
+    public static void reduce(RunREDUCE runREDUCE) {
+        try {
+            ProcessBuilder pb =
+                new ProcessBuilder("D:\\Program Files\\Reduce\\lib\\psl\\psl\\bpsl.exe",
+                                   "-td", "1000", "-f",
+                                   "D:\\Program Files\\Reduce\\lib\\psl\\red\\reduce.img");
+            pb.redirectErrorStream(true);
+            // pb.redirectInput(ProcessBuilder.Redirect.INHERIT); // Works!
+            Process p = pb.start();
+
+            // Assign the REDUCE input stream to a global variable:
+            OutputStreamWriter osr = new OutputStreamWriter(p.getOutputStream());
+            reduceInputPrintWriter = new PrintWriter(osr);
+
+            // Start a thread to handle the REDUCE output stream:
+            ReduceOutputThread outputGobbler = new 
+                ReduceOutputThread(p.getInputStream(), runREDUCE.outputTextArea);
+            outputGobbler.start();
+
+            // } catch (IOException | InterruptedException e) {}
+        } catch (Exception e) {}
+    }
+
+    public static void main(String... args) {
+        // Schedule a job for the event-dispatching thread:
+        // creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
+
+
+/**
+ * This thread reads from the REDUCE output pipe and appends it to the
+ * GUI output pane.
+ */ 
+class ReduceOutputThread extends Thread {
+    InputStream input;        // REDUCE pipe output (buffered)
+    JTextArea outputTextArea; // GUI output pane
+
+    ReduceOutputThread(InputStream input, JTextArea outputTextArea) {
+        this.input = input;
+        this.outputTextArea = outputTextArea;
+    }
+
+    public void run() {
+        // Must output characters rather than lines so that prompt appears!
+        try (InputStreamReader isr = new InputStreamReader(input);
+             BufferedReader br = new BufferedReader(isr)) {
+            int c;
+            for (;;) {
+                if (!br.ready())
+                    Thread.sleep(10);
+                else if ((c = br.read()) != -1) {
+                    outputTextArea.append(String.valueOf((char) c));
+                    // outputTextArea.setCaretPosition(outputTextArea.getDocument().getLength());
+                }
+                else break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
