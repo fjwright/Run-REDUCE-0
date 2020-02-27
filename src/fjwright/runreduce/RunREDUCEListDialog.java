f6b9c879cfbd7993a7b6c63e078d79b3d25e72f2
Francis Wright <59773998+fjwright@users.noreply.github.com>
2020-02-27 16:02:40 +0000

    Make ShutOutputFilesDialog and LoadPackagesDialog both inherit from the same abstract generic class, which seems to work OK apart from an unchecked cast warning.

--- src/fjwright/runreduce/RunREDUCEListDialog.java
+++ src/fjwright/runreduce/RunREDUCEListDialog.java
@@ -1,37 +1,31 @@
 package fjwright.runreduce;
 
-import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
-import java.io.*;
-import java.util.List;          // Also in java.awt!
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
-
+import java.util.List;
 
 /**
  * This class provides a modal dialog to select open output files to
- * shut.
+ * shut or packages to load.
  */
-class ShutOutputFilesDialog extends JDialog implements ActionListener {
+abstract class RunREDUCEListDialog<E> extends JDialog implements ActionListener {
     protected Frame frame;
-    private static JList<File> list = new JList<>();
-    private static int[] fileIndices;
+    protected JList<E> list = new JList<>();
 
-    ShutOutputFilesDialog(Frame frame) {
+    RunREDUCEListDialog(Frame frame, String dialogTitle, String listLabel, String buttonText) {
         // Create a modal dialog:
-        super(frame, "Shut Output Files...", true);
+        super(frame, dialogTitle, true);
         this.frame = frame;
-
-        list.setLayoutOrientation(JList.VERTICAL);
-        list.setVisibleRowCount(5);
 
         JScrollPane listScroller = new JScrollPane(list);
         listScroller.setAlignmentX(Component.LEFT_ALIGNMENT);
 
         JPanel listPane = new JPanel();
         listPane.setLayout(new BoxLayout(listPane, BoxLayout.PAGE_AXIS));
-        JLabel label = new JLabel
-                ("Select one or more open output files to shut...");
+        JLabel label = new JLabel(listLabel);
         label.setLabelFor(list);
         listPane.add(label);
         listPane.add(Box.createRigidArea(new Dimension(0, 5)));
@@ -39,8 +33,8 @@ class ShutOutputFilesDialog extends JDialog implements ActionListener {
         listPane.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
         // Create and initialize the control buttons:
-        JButton shutButton = new JButton("Shut");
-        shutButton.setActionCommand("Shut");
+        JButton shutButton = new JButton(buttonText);
+        shutButton.setActionCommand(buttonText);
         shutButton.addActionListener(this);
         JButton cancelButton = new JButton("Cancel");
         cancelButton.addActionListener(this);
@@ -58,9 +52,26 @@ class ShutOutputFilesDialog extends JDialog implements ActionListener {
         add(listPane, BorderLayout.CENTER);
         add(buttonPane, BorderLayout.PAGE_END);
     }
+}
+
+/**
+ * This class provides a modal dialog to select open output files to
+ * shut.
+ */
+class ShutOutputFilesDialog<File> extends RunREDUCEListDialog<File> {
+    private static int[] fileIndices;
+
+    ShutOutputFilesDialog(Frame frame) {
+        super(frame,
+                "Shut Output Files...",
+                "Select one or more open output files to shut...",
+                "Shut");
+        list.setLayoutOrientation(JList.VERTICAL);
+        list.setVisibleRowCount(5);
+    }
 
     int[] showDialog(List<File> outputFileList) {
-        list.setListData(outputFileList.toArray(new File[0]));
+        list.setListData((File[]) outputFileList.toArray());
         pack();                       // must be done dynamically
         setLocationRelativeTo(frame); // ditto
         setVisible(true);
@@ -76,55 +87,24 @@ class ShutOutputFilesDialog extends JDialog implements ActionListener {
     }
 }
 
-class LoadPackagesDialog extends JDialog implements ActionListener {
-    protected Frame frame;
-    private static JList<String> list = new JList<>();
-    private static List<String> selectedPackages;
+/**
+ * This class provides a modal dialog to select packages to load.
+ */
+class LoadPackagesDialog<String> extends RunREDUCEListDialog<String> {
+    private List<String> selectedPackages;
 
     LoadPackagesDialog(Frame frame) {
         // Create a modal dialog:
-        super(frame, "Load Packages...", true);
-        this.frame = frame;
-
+        super(frame,
+                "Load Packages...",
+                "Select one or more packages to load...",
+                "Load");
         list.setLayoutOrientation(JList.HORIZONTAL_WRAP);
         list.setVisibleRowCount(20);
-
-        JScrollPane listScroller = new JScrollPane(list);
-        listScroller.setAlignmentX(Component.LEFT_ALIGNMENT);
-
-        JPanel listPane = new JPanel();
-        listPane.setLayout(new BoxLayout(listPane, BoxLayout.PAGE_AXIS));
-        JLabel label = new JLabel
-                ("Select one or more packages to load...");
-        label.setLabelFor(list);
-        listPane.add(label);
-        listPane.add(Box.createRigidArea(new Dimension(0, 5)));
-        listPane.add(listScroller);
-        listPane.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-
-        // Create and initialize the control buttons:
-        JButton shutButton = new JButton("Load");
-        shutButton.setActionCommand("Load");
-        shutButton.addActionListener(this);
-        JButton cancelButton = new JButton("Cancel");
-        cancelButton.addActionListener(this);
-
-        // Lay out the buttons horizontally and right-justified:
-        JPanel buttonPane = new JPanel();
-        buttonPane.setLayout(new BoxLayout(buttonPane, BoxLayout.LINE_AXIS));
-        buttonPane.setBorder(BorderFactory.createEmptyBorder(0, 10, 10, 10));
-        buttonPane.add(Box.createHorizontalGlue());
-        buttonPane.add(shutButton);
-        buttonPane.add(Box.createRigidArea(new Dimension(10, 0)));
-        buttonPane.add(cancelButton);
-
-        // Lay out the dialog contents:
-        add(listPane, BorderLayout.CENTER);
-        add(buttonPane, BorderLayout.PAGE_END);
     }
 
     List<String> showDialog(List<String> packageList) {
-        list.setListData(packageList.toArray(new String[0]));
+        list.setListData((String[]) packageList.toArray());
         pack();                       // must be done dynamically
         setLocationRelativeTo(frame); // ditto
         setVisible(true);
@@ -135,7 +115,7 @@ class LoadPackagesDialog extends JDialog implements ActionListener {
         if ("Load".equals(e.getActionCommand())) {
             selectedPackages = list.getSelectedValuesList();
         } else
-            selectedPackages = new ArrayList<String>();
+            selectedPackages = new ArrayList<>();
         setVisible(false);
     }
 }
