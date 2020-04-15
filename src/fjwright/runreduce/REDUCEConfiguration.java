ba86c59571c56736bb098c6671b1a1eea22fdc8b
Francis Wright <59773998+fjwright@users.noreply.github.com>
2020-04-15 11:41:12 +0100

    Move classes from REDUCECommand.java into RRPreferences.java renamed to REDUCEConfiguration.java. Delete REDUCECommand.java.

--- src/fjwright/runreduce/REDUCEConfiguration.java
+++ src/fjwright/runreduce/REDUCEConfiguration.java
@@ -1,15 +1,20 @@
 package fjwright.runreduce;
 
 import javax.swing.text.StyleConstants;
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class RRPreferences {
+class RRPreferences {
     static final boolean windowsOS = System.getProperty("os.name").startsWith("Windows");
     static final Preferences prefs = Preferences.userRoot().node("/fjwright/runreduce");  // cf. package name
     // On Windows, Java stores the preferences for this application in the registry under the key
@@ -76,7 +81,46 @@ public class RRPreferences {
     }
 }
 
-/*
+/**
+ * This class defines a command to run REDUCE after checking it is executable.
+ */
+class REDUCECommand {
+    String version = ""; // e.g. "CSL REDUCE" or "PSL REDUCE"
+    String versionRootDir = ""; // version-specific reduceRootDir.
+    String[] command = {"", "", "", "", "", ""}; // executable pathname followed by arguments
+
+    REDUCECommand() {
+    }
+
+    REDUCECommand(String version, String versionRootDir, String... command) {
+        this.version = version;
+        this.versionRootDir = versionRootDir;
+        this.command = command;
+    }
+
+    // Merge this method into the constructor?
+    String[] buildCommand() {
+        // Replace $REDUCE by versionRootDir if non-null else by reduceRootDir.
+        Path reduceRootPath = Paths.get(
+                !versionRootDir.equals("") ? versionRootDir : RunREDUCE.reduceConfiguration.reduceRootDir);
+        String[] command = new String[this.command.length];
+        for (int i = 0; i < this.command.length; i++) {
+            String element = this.command[i];
+            if (element.startsWith("$REDUCE/"))
+                element = reduceRootPath.resolve(element.substring(8)).toString();
+            command[i] = element;
+        }
+        if (!Files.isExecutable(Paths.get(command[0]))) {
+            RunREDUCE.errorMessageDialog(
+                    command[0] + " is not executable!",
+                    "REDUCE Configuration Error");
+            return null;
+        }
+        return command;
+    }
+}
+
+/**
  * This class defines a list of commands to run different versions of REDUCE.
  */
 class REDUCECommandList extends ArrayList<REDUCECommand> {
@@ -97,7 +141,7 @@ abstract class REDUCEConfigurationType {
     REDUCECommandList reduceCommandList;
 }
 
-/*
+/**
  * This class represents the application default REDUCE directory and command configuration.
  * It is initialised when the application starts.
  * **Note that no value can be null because preference values cannot be null.**
@@ -240,5 +284,50 @@ class REDUCEConfiguration extends REDUCEConfigurationType {
                 prefs.put(ARG + i, cmd.command[i]);
             prefs = prefs.parent();
         }
+    }
+}
+
+/**
+ * This class provides a list of all REDUCE packages by parsing the package.map file.
+ * The list excludes preloaded packages, and it is sorted alphabetically.
+ */
+class REDUCEPackageList extends ArrayList<String> {
+
+    REDUCEPackageList() {
+        Path packagesRootPath = Paths.get(RunREDUCE.reduceConfiguration.packagesRootDir);
+        Path packageMapFile = packagesRootPath.resolve("packages/package.map");
+        if (!Files.isReadable(packageMapFile)) {
+            RunREDUCE.errorMessageDialog(
+                    new String[]{"The REDUCE package map file is not available!",
+                            "Please correct 'Packages Root Dir' in the 'Configure REDUCE...' dialogue,",
+                            "which will open automatically when you close this dialogue."},
+                    "REDUCE Package Error");
+            RRMenuBar.showREDUCEConfigDialog();
+            return;
+        }
+
+        try (BufferedReader reader = Files.newBufferedReader(packageMapFile)) {
+            String line;
+            Pattern pattern = Pattern.compile("\\s*\\((\\w+)");
+            // The preloaded packages are these (using non-capturing groups):
+            Pattern exclude = Pattern.compile("(?:alg)|(?:arith)|(?:mathpr)|(?:poly)|(?:rlisp)");
+            while ((line = reader.readLine()) != null) {
+                Matcher matcher = pattern.matcher(line);
+                if (matcher.lookingAt()) {
+                    String pkg = matcher.group(1);
+                    if (!exclude.matcher(pkg).matches()) this.add(pkg);
+                }
+            }
+        } catch (IOException x) {
+            System.err.format("IOException: %s%n", x);
+        }
+
+        Collections.sort(this);
+
+        // For testing only:
+//        for (String s : this) {
+//            System.out.print(s);
+//            System.out.print(" ");
+//        }
     }
 }
